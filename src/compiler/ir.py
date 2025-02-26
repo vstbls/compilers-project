c8bd4c6b598b936bdb584d1698eafd25bc58978f
vstbls <7550178+vstbls@users.noreply.github.com>
2025-02-26 22:25:34 +0200

    IR classes

---
+++ src/compiler/ir.py
@@ -0,0 +1,71 @@
+from dataclasses import dataclass
+import dataclasses
+from typing import Any
+from compiler.classes import Location
+
+@dataclass(frozen=True)
+class IRVar:
+    name: str
+
+    def __str__(self) -> str:
+        return self.name
+
+
+@dataclass(frozen=True)
+class Instruction:
+    location: Location
+    
+    def __str__(self) -> str:
+        def format_value(v: Any) -> str:
+            if isinstance(v, list):
+                return f'[{", ".join(format_value(e) for e in v)}]'
+            else:
+                return str(v)
+        args = ', '.join(
+            format_value(getattr(self, field.name))
+            for field in dataclasses.fields(self)
+            if field.name != 'location'
+        )
+        return f'{type(self).__name__}({args})'
+
+
+@dataclass(frozen=True)
+class Label(Instruction):
+    name: str
+
+
+@dataclass(frozen=True)
+class LoadBoolConst(Instruction):
+    value: bool
+    dest: IRVar
+
+
+@dataclass(frozen=True)
+class LoadIntConst(Instruction):
+    value: bool
+    dest: IRVar
+
+
+@dataclass(frozen=True)
+class Copy(Instruction):
+    source: IRVar
+    dest: IRVar
+
+
+@dataclass(frozen=True)
+class Call(Instruction):
+    fun: IRVar
+    args: list[IRVar]
+    dest: IRVar
+
+
+@dataclass(frozen=True)
+class Jump(Instruction):
+    label: Label
+
+
+@dataclass(frozen=True)
+class CondJump(Instruction):
+    cond: IRVar
+    then_label: Label
+    else_label: Label
